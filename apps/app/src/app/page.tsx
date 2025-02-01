6f14e31d133a15668a8880e2fb11917b63b4b66a
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2025-02-02 02:56:24 +0600

    🚚 Move home page out of authorized layout

---
+++ apps/app/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="grid min-h-screen place-content-center">
+      <h1 className="bg-gradient-to-r from-rose-500 via-pink-500 to-blue-500 bg-clip-text font-mono text-6xl font-extrabold tracking-tighter text-transparent">
+        Next Zen Commerce
+      </h1>
+    </div>
+  );
+}
