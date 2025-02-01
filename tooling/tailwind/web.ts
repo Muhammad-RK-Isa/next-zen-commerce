c4e852d29ba39a4cfb52046076d5714aa0d6bd8f
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2025-02-01 12:52:17 +0600

    feat: add bunch of animations and background images

--- tooling/tailwind/web.ts
+++ tooling/tailwind/web.ts
@@ -12,7 +12,6 @@ import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 import base from "./base";
 
 export default {
-  darkMode: ["class"],
   content: base.content,
   presets: [base],
   theme: {
@@ -24,27 +23,66 @@ export default {
       },
     },
     extend: {
+      backgroundImage: {
+        "dots-pattern": "radial-gradient(transparent 1px, #fafafa 1px)",
+        "dots-pattern-dark": "radial-gradient(transparent 1px, #000 1px)",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordion-down": {
+        aurora: {
           from: {
-            height: "0",
+            backgroundPosition: "50% 50%, 50% 50%",
           },
           to: {
-            height: "var(--radix-accordion-content-height)",
+            backgroundPosition: "350% 50%, 350% 50%",
           },
         },
-        "accordion-up": {
-          from: {
-            height: "var(--radix-accordion-content-height)",
-          },
+        "collapsible-down": {
+          from: { height: "0", opacity: "0" },
           to: {
-            height: "0",
+            height: "var(--radix-collapsible-content-height)",
           },
+        },
+        "collapsible-up": {
+          from: {
+            height: "var(--radix-collapsible-content-height)",
+          },
+          to: { height: "0", opacity: "0" },
+        },
+        "accordion-down": {
+          from: { height: "0", opacity: "0.5" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0", opacity: "0.5" },
+        },
+        "dialog-in": {
+          from: { transform: "scale(0.95) translate(-50%, 0)", opacity: "0" },
+          to: { transform: "scale(1) translate(-50%, 0)" },
+        },
+        "dialog-out": {
+          from: { transform: "scale(1) translate(-50%, 0)" },
+          to: { transform: "scale(0.95) translateY(-50%, 0)", opacity: "0" },
+        },
+        "popover-in": {
+          from: { opacity: "0", transform: "scale(0.98) translateY(-4px)" },
+          to: { opacity: "1", transform: "scale(1) translateY(0)" },
+        },
+        "popover-out": {
+          from: { opacity: "1", transform: "translateY(0)" },
+          to: { opacity: "0", transform: "translateY(-4px)" },
+        },
+        "fade-in": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        "fade-out": {
+          to: { opacity: "0" },
         },
         blink: {
           "0%": {
@@ -72,8 +110,18 @@ export default {
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        aurora: "aurora 60s linear infinite",
+        "fade-in": "fade-in 300ms ease",
+        "fade-out": "fade-out 300ms ease",
+        "dialog-in": "dialog-in 200ms cubic-bezier(0.32, 0.72, 0, 1)",
+        "dialog-out": "dialog-out 300ms cubic-bezier(0.32, 0.72, 0, 1)",
+        "popover-in": "popover-in 150ms ease",
+        "popover-out": "popover-out 150ms ease",
+        "collapsible-down": "collapsible-down 150ms ease-out",
+        "collapsible-up": "collapsible-up 150ms ease-out",
+        "accordion-down": "accordion-down 200ms ease-out",
+        "accordion-up": "accordion-up 200ms ease-out",
+
         blink: "blink 1.4s both infinite",
         spinner: "spinner 1.2s linear infinite",
         shimmer: "shimmer 1.5s infinite",
