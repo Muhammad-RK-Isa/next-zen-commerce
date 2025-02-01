932808892a1561260fae6224b0fce811c7598c0b
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2025-02-01 12:55:49 +0600

    feat: add internal env package

---
+++ packages/env/src/index.ts
@@ -0,0 +1,29 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "production", "test"])
+      .default("development"),
+    ARCJET_KEY: z.string().startsWith("ajkey_"),
+    DATABASE_URL: z.string().url(),
+    EMAIL_FROM: z.string(),
+    EMAIL_CONTACT: z.string().email(),
+    EMAIL_SUPPORT: z.string().email(),
+    EMAIL_HOST: z.string(),
+    EMAIL_PORT: z.number({ coerce: true }),
+    EMAIL_USERNAME: z.string(),
+    EMAIL_PASSWORD: z.string(),
+    UPLOADTHING_TOKEN: z.string(),
+    REDIS_URL: z.string().url(),
+  },
+  clientPrefix: "PUBLIC_",
+  client: {
+    PUBLIC_APP_URL: z.string().url(),
+    PUBLIC_STORE_URL: z.string().url(),
+    PUBLIC_ADMIN_URL: z.string().url(),
+  },
+  runtimeEnv: process.env,
+  emptyStringAsUndefined: true,
+});
