91e7944801a5bc740ca49dcb4e6a87ae1899af2d
muhammad-rk-isa <muhammad.rk.isa@gmail.com>
2025-03-16 20:59:48 +0600

    chore: validate prettier integration

--- apps/merchant/app/page.tsx
+++ apps/merchant/app/page.tsx
@@ -3,7 +3,7 @@ import { siteConfig } from '~/config/site';
 export default function Page() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center">
-      <h1 className="bg-gradient-to-r from-rose-500 via-pink-500 to-blue-500 bg-clip-text font-bold font-mono text-2xl text-transparent tracking-tighter transition-all duration-300 hover:tracking-normal sm:text-6xl lg:text-7xl">
+      <h1 className="bg-gradient-to-r from-rose-500 via-pink-500 to-blue-500 bg-clip-text font-mono text-2xl font-bold tracking-tighter text-transparent transition-all duration-300 hover:tracking-normal sm:text-6xl lg:text-7xl">
         {siteConfig.name}
       </h1>
     </div>
